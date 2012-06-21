e08d485c454955770b302944fd79902289e6ff53
daan-perceptum <daan@perceptum.nl>
2012-06-21 17:56:40 +0300

    Update multi-language

--- exercises/converting_decimals_to_fractions_2.lang.js
+++ exercises/converting_decimals_to_fractions_2.lang.js
@@ -1,11 +1,11 @@
 ({
 	"nl" : {
-		"question1"	: 'Express <code><var>D</var></code> as a fraction.',
-		"question2"	: 'Express <code><var>FIXED</var></code> as a mixed number. Reduce to lowest terms.',
+		"question1"	: 'Schrijf <code><var>D</var></code> als een breuk.',
+		"question2"	: 'Druk <code><var>FIXED</var></code> uit als een gemengde breuk.',
 		
-		"hint1"		: '<code><var>D</var></code> can be represented as follows.',
-		"hint2"		: 'You can also skip a few steps by making a fraction with <code><var>NUMERATOR</var></code> as the numerator and <code>10000</code> (because the decimal extends to the ten thousandths place) as the denominator.',
-		"hint3"		: '<code><var>FIXED</var></code> can be represented as follows:',
-		"hint4"		: 'Reduce the fractional part by dividing the numerator and denominator by <code><var>GCD</var></code>.'
+		"hint1"		: '<code><var>D</var></code> kan als volgt weergegeven worden:',
+		"hint2"		: 'e kunt ook een paar stappen overslaan door deze als een breuk te schrijven met als teller <code><var>NUMERATOR</var></code> en als noemer <code>10000</code> , omdat het getal achter de komma zich uitstrekt tot de tienduizendste plaats).',
+		"hint3"		: '<code><var>FIXED</var></code>  kan als volgt weergegeven worden:',
+		"hint4"		: 'Vervolgens maak je de breuk kleiner door zowel de teller als de noemer door <code><var>GCD</var></code> te delen.'
 		}
 })
