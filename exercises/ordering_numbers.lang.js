bbc6ce577f4e6c60ce03f6b95b7301a1bb0eb2d2
Eveline <eveline@perceptum.nl>
2012-11-13 11:18:57 +0100

    Update exercises/ordering_numbers.lang.js

--- exercises/ordering_numbers.lang.js
+++ exercises/ordering_numbers.lang.js
@@ -3,8 +3,8 @@
 		"question1"	: 'Orden de getallen van klein naar groot:',
 		"instruction1"	: 'Sleep de getallen zo dat ze van links naar recht geordend zijn van klein naar groot:',
 		
-		"hint1"		: 'Converteer elk getal naar een decimaal.',
+		"hint1"		: 'Zet elk getal om naar een decimaal.',
 		"hint2"		: 'Sorteer de decimalen: <code><var>NUMS_SORT_DECS</var></code>.',
-		"hint3"		: 'Converteer weer terug: <code>\\Large{<var>NUMS_SORT_TEX</var>}</code>.'
+		"hint3"		: 'Zet elk getal weer terug naar hun oude vorm: <code>\\Large{<var>NUMS_SORT_TEX</var>}</code>.'
 		}
 })
