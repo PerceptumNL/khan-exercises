2e4cd007e913746e68be8e9ca7af74a3b7994e3f
Eveline <eveline@perceptum.nl>
2012-11-22 15:40:28 +0100

    Update exercises/direct_and_inverse_variation.lang.js

--- exercises/direct_and_inverse_variation.lang.js
+++ exercises/direct_and_inverse_variation.lang.js
@@ -1,35 +1,35 @@
 ({
 	"nl" : {
-		"question1"	: 'Which of these equations could represent the relationship between <code><var>V1</var></code> and <code><var>V2</var></code>?',
-		"question3"	: 'Which of these equations could represent the relationship between <code><var>V1</var></code> and <code><var>V2</var></code>?',
+		"question1"	: 'Welke van deze vergelijkingen kunnen de relatie tussen <code><var>V1</var></code> en <code><var>V2</var></code> vertegenwoordigen?',
+		"question3"	: 'Welke van deze vergelijkingen kunnen de relatie tussen <code><var>V1</var></code> en <code><var>V2</var></code> vertegenwoordigen?',
 		
-		"hint1"		: '<var>STATEMENT</var> if <code><var>V1</var> = k \cdot <var>V2</var></code> for some constant k',
-		"hint2"		: '<code><var>V1</var> = <var>MULTIPLIER</var> \cdot <var>V2</var></code> fits this pattern, with <code>k = <var>MULTIPLIER</var></code>.',
-		"hint3"		: '<var>STATEMENT</var> if <code><var>V1</var> = k \cdot <var>V2</var></code> for some constant k',
-		"hint4"		: 'If you divide each side of this expression by <code><var>V2</var></code>, you get <code>\dfrac{<var>V1</var>}{<var>V2</var>} = k</code> for some constant <code>k</code>.',
-		"hint5"		: '<code>\dfrac{<var>V1</var>}{<var>V2</var>} = <var>MULTIPLIER</var></code> fits this pattern, with <code>k = <var>MULTIPLIER</var></code>.',
-		"hint6"		: '<var>STATEMENT</var> if <code><var>V1</var> = k \cdot <var>V2</var></code> for some constant k',
-		"hint7"		: 'If you divide each side of this expression by <code>k</code>, you get <code>\dfrac{1}{k} \cdot <var>V1</var> = <var>V2</var></code>.',
-		"hint8"		: '<code><var>MULTIPLIER</var> \cdot <var>V1</var> = <var>V2</var></code> fits this pattern, with <code>k = <var>MULTIPLIER_INVERSE</var></code>.',
+		"hint1"		: '<var>STATEMENT</var> Als <code><var>V1</var> = k \cdot <var>V2</var></code> voor een constante k',
+		"hint2"		: '<code><var>V1</var> = <var>MULTIPLIER</var> \cdot <var>V2</var></code> past in dit patroon, met <code>k = <var>MULTIPLIER</var></code>.',
+		"hint3"		: '<var>STATEMENT</var> als <code><var>V1</var> = k \cdot <var>V2</var></code> voor een constante k',
+		"hint4"		: 'Wanneer je beide kanten van deze vergelijking deelt door <code><var>V2</var></code>, krijg je <code>\dfrac{<var>V1</var>}{<var>V2</var>} = k</code> voor een constante <code>k</code>.',
+		"hint5"		: '<code>\dfrac{<var>V1</var>}{<var>V2</var>} = <var>MULTIPLIER</var></code> past in dit patroon, met<code>k = <var>MULTIPLIER</var></code>.',
+		"hint6"		: '<var>STATEMENT</var> als <code><var>V1</var> = k \cdot <var>V2</var></code> voor een constante k',
+		"hint7"		: 'Wanneer je beide kanten van deze vergelijking deelt door <code>k</code>, krijg je <code>\dfrac{1}{k} \cdot <var>V1</var> = <var>V2</var></code>.',
+		"hint8"		: '<code><var>MULTIPLIER</var> \cdot <var>V1</var> = <var>V2</var></code> past in dit patroon, met <code>k = <var>MULTIPLIER_INVERSE</var></code>.',
 		"hint9"		: 'randFromArray(['+
-						'"&lt;code&gt;&lt;var&gt;V1&lt;/var&gt;&lt;/code&gt; is inversely proportional to &lt;code&gt;&lt;var&gt;V2&lt;/var&gt;&lt;/code&gt;",'+
-						'"&lt;code&gt;&lt;var&gt;V1&lt;/var&gt;&lt;/code&gt; and &lt;code&gt;&lt;var&gt;V2&lt;/var&gt;&lt;/code&gt; vary inversely",'+
-						'"&lt;code&gt;&lt;var&gt;V1&lt;/var&gt;&lt;/code&gt; varies inversely with &lt;code&gt;&lt;var&gt;V2&lt;/var&gt;&lt;/code&gt;",'+
-						'"&lt;code&gt;&lt;var&gt;V1&lt;/var&gt;&lt;/code&gt; and &lt;code&gt;&lt;var&gt;V2&lt;/var&gt;&lt;/code&gt; are in inverse variation"'+
+						'"&lt;code&gt;&lt;var&gt;V1&lt;/var&gt;&lt;/code&gt; is omgekeerd evenredig met &lt;code&gt;&lt;var&gt;V2&lt;/var&gt;&lt;/code&gt;",'+
+						'"&lt;code&gt;&lt;var&gt;V1&lt;/var&gt;&lt;/code&gt; en &lt;code&gt;&lt;var&gt;V2&lt;/var&gt;&lt;/code&gt; vary inversely",'+
+						'"&lt;code&gt;&lt;var&gt;V1&lt;/var&gt;&lt;/code&gt; a varieert omgekeerd met &lt;code&gt;&lt;var&gt;V2&lt;/var&gt;&lt;/code&gt;",'+
+						'"&lt;code&gt;&lt;var&gt;V1&lt;/var&gt;&lt;/code&gt; en &lt;code&gt;&lt;var&gt;V2&lt;/var&gt;&lt;/code&gt; zijn in directe variatie"'+
 					'])',
-		"hint10"	: '<var>STATEMENT</var> if <code><var>V1</var> = k \cdot \dfrac{1}{<var>V2</var>}</code> for some constant k',
-		"hint11"	: '<var>STATEMENT</var> if <code><var>V1</var> = k \cdot \dfrac{1}{<var>V2</var>}</code> for some constant k',
-		"hint12"	: 'If you multiply each side of this expression by <code><var>V2</var></code>, you get <code><var>V1</var> \cdot <var>V2</var> = k</code> for some constant <code>k</code>.',
-		"hint13"	: '<code><var>V1</var> \cdot <var>V2</var> = <var>MULTIPLIER</var></code> fits this pattern, with <code>k = <var>MULTIPLIER</var></code>.',
-		"hint14"	: '<var>STATEMENT</var> if <code><var>V1</var> = k \cdot \dfrac{1}{<var>V2</var>}</code> for some constant k',
-		"hint15"	: 'If you divide each side of this expression by <code>k</code>, you get <code>\dfrac{<var>V1</var>}{k} = \dfrac{1}{<var>V2</var>}</code>.',
-		"hint16"	: 'Then you can take the inverse of each side to get <code>\dfrac{k}{<var>V1</var>} = <var>V2</var></code>.',
-		"hint17"	: '<code><var>MULTIPLIER</var> \cdot \dfrac{1}{<var>V1</var>} = <var>V2</var></code> fits this pattern, with <code>k = <var>MULTIPLIER</var></code>.',
+		"hint10"	: '<var>STATEMENT</var> als <code><var>V1</var> = k \cdot \dfrac{1}{<var>V2</var>}</code> voor een constante k',
+		"hint11"	: '<var>STATEMENT</var> als <code><var>V1</var> = k \cdot \dfrac{1}{<var>V2</var>}</code> voor een constante k',
+		"hint12"	: 'Als u elke kant van deze vergelijking met <code><var>V2</var></code> vermenigvuldigt, krijg je  <code><var>V1</var> \cdot <var>V2</var> = k</code> voor een constante <code>k</code>.',
+		"hint13"	: '<code><var>V1</var> \cdot <var>V2</var> = <var>MULTIPLIER</var></code> past in dit patroon, met <code>k = <var>MULTIPLIER</var></code>.',
+		"hint14"	: '<var>STATEMENT</var> als <code><var>V1</var> = k \cdot \dfrac{1}{<var>V2</var>}</code> voor een constante k',
+		"hint15"	: 'Wanneer je beide kanten van deze vergelijking deelt door <code>k</code>, krijg je <code>\dfrac{<var>V1</var>}{k} = \dfrac{1}{<var>V2</var>}</code>.',
+		"hint16"	: 'Dan kun je van beide kanten de inverse nemen om <code>\dfrac{k}{<var>V1</var>} = <var>V2</var></code> te krijgen.',
+		"hint17"	: '<code><var>MULTIPLIER</var> \cdot \dfrac{1}{<var>V1</var>} = <var>V2</var></code> past in dit patroon, met <code>k = <var>MULTIPLIER</var></code>.',
 		"hint18"	: 'randFromArray(['+
-						'"&lt;code&gt;&lt;var&gt;V1&lt;/var&gt;&lt;/code&gt; is directly proportional to &lt;code&gt;&lt;var&gt;V2&lt;/var&gt;&lt;/code&gt;",'+
-						'"&lt;code&gt;&lt;var&gt;V1&lt;/var&gt;&lt;/code&gt; and &lt;code&gt;&lt;var&gt;V2&lt;/var&gt;&lt;/code&gt; vary directly",'+
-						'"&lt;code&gt;&lt;var&gt;V1&lt;/var&gt;&lt;/code&gt; varies directly with &lt;code&gt;&lt;var&gt;V2&lt;/var&gt;&lt;/code&gt;",'+
-						'"&lt;code&gt;&lt;var&gt;V1&lt;/var&gt;&lt;/code&gt; and &lt;code&gt;&lt;var&gt;V2&lt;/var&gt;&lt;/code&gt; are in direct variation"'+
+						'"&lt;code&gt;&lt;var&gt;V1&lt;/var&gt;&lt;/code&gt; is recht evenredig met &lt;code&gt;&lt;var&gt;V2&lt;/var&gt;&lt;/code&gt;",'+
+						'"&lt;code&gt;&lt;var&gt;V1&lt;/var&gt;&lt;/code&gt; en &lt;code&gt;&lt;var&gt;V2&lt;/var&gt;&lt;/code&gt; vary directly",'+
+						'"&lt;code&gt;&lt;var&gt;V1&lt;/var&gt;&lt;/code&gt; varieert omgekeerd met &lt;code&gt;&lt;var&gt;V2&lt;/var&gt;&lt;/code&gt;",'+
+						'"&lt;code&gt;&lt;var&gt;V1&lt;/var&gt;&lt;/code&gt; en &lt;code&gt;&lt;var&gt;V2&lt;/var&gt;&lt;/code&gt; zijn in directe variatie"'+
 					'])'
 		}
 })
