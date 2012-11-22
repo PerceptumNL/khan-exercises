fd368cfe469942a4b5218b7c996675457f7e3173
Eveline <eveline@perceptum.nl>
2012-11-22 15:46:49 +0100

    Update exercises/even_and_odd_functions.lang.js

--- exercises/even_and_odd_functions.lang.js
+++ exercises/even_and_odd_functions.lang.js
@@ -1,16 +1,16 @@
 ({
 	"nl" : {
-		"question1"	: '<code>f(x)</code> is graphed below.',
-		"question2"	: 'Is <code>f(x)</code> even, odd, or neither?',
+		"question1"	: 'De grafiek van <code>f(x)</code> staat hieronder.',
+		"question2"	: 'Is <code>f(x)</code> even, oneven of geen van beide?',
 		"question3"	: 'Even',
-		"question4"	: 'Odd',
-		"question5"	: 'Neither',
+		"question4"	: 'oneven',
+		"question5"	: 'geen van beide',
 		
-		"hint1"		: '<code>f(-a)\neq f(a)</code>, so <code>f(x)</code> is not even.',
-		"hint2"		: '<code>f(-a)\neq -f(a)</code>, so <code>f(x)</code> is not odd.',
-		"hint3"		: '<code>f(-a)=-f(a)</code> for all of these points, so <code>f(x)</code> is... ?',
-		"hint4"		: '<code>f(-a)=-f(a)</code> for all of these points, so <code>f(x)</code> is <span class="hint_blue">odd</span>.',
-		"hint5"		: '<code>f(-a)=f(a)</code> for all of these points, so <code>f(x)</code> is... ?',
-		"hint6"		: '<code>f(-a)=f(a)</code> for all of these points, so <code>f(x)</code> is <span class="hint_blue">even</span>.'
+		"hint1"		: '<code>f(-a)\neq f(a)</code>, dus <code>f(x)</code> is niet even.',
+		"hint2"		: '<code>f(-a)\neq -f(a)</code>, dus <code>f(x)</code> is niet oneven odd.',
+		"hint3"		: '<code>f(-a)=-f(a)</code> voor alle punten, dus <code>f(x)</code> is... ?',
+		"hint4"		: '<code>f(-a)=-f(a)</code> voor alle punten, dus <code>f(x)</code> is <span class="hint_blue">odd</span>.',
+		"hint5"		: '<code>f(-a)=f(a)</code> voor alle punten, dus <code>f(x)</code> is... ?',
+		"hint6"		: '<code>f(-a)=f(a)</code> voor alle punten, dus <code>f(x)</code> is <span class="hint_blue">even</span>.'
 		}
 })
